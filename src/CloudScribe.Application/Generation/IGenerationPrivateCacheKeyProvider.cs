2450d014f933e3636ad709703c8ca3838cbe8c65
sohweiyu2023 <134089475+sohweiyu2023@users.noreply.github.com>
2026-08-23 10:46:51 +0800

    Stage5 v2.23 add cache key material boundary

---
+++ src/CloudScribe.Application/Generation/IGenerationPrivateCacheKeyProvider.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+
+namespace CloudScribe.Application.Generation;
+
+public interface IGenerationPrivateCacheKeyProvider
+{
+    ValueTask<GenerationPrivateCacheKeyMaterial> GetOrCreateAsync(CancellationToken cancellationToken = default);
+}
+
+public sealed class GenerationPrivateCacheKeyMaterial : IDisposable
+{
+    private byte[]? _buffer;
+
+    public GenerationPrivateCacheKeyMaterial(byte[] buffer)
+    {
+        ArgumentNullException.ThrowIfNull(buffer);
+        if (buffer.Length < 32)
+        {
+            CryptographicOperations.ZeroMemory(buffer);
+            throw new ArgumentException("Private cache HMAC key material must contain at least 256 bits.", nameof(buffer));
+        }
+
+        _buffer = buffer;
+    }
+
+    public ReadOnlySpan<byte> Span => _buffer ?? throw new ObjectDisposedException(nameof(GenerationPrivateCacheKeyMaterial));
+
+    public void Dispose()
+    {
+        var buffer = Interlocked.Exchange(ref _buffer, null);
+        if (buffer is not null)
+        {
+            CryptographicOperations.ZeroMemory(buffer);
+        }
+    }
+}
