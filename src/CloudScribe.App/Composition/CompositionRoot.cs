51bcca63de5b730d1554b86f66af8283977fea51
sohweiyu2023 <134089475+sohweiyu2023@users.noreply.github.com>
2026-09-04 08:27:36 +0800

    fix(stage6): keep production composition within analyzer limits

--- src/CloudScribe.App/Composition/CompositionRoot.cs
+++ src/CloudScribe.App/Composition/CompositionRoot.cs
@@ -32,57 +32,77 @@ public static class CompositionRoot
         builder.Logging.SetMinimumLevel(LogLevel.Information);
 
         builder.Services.AddCloudScribeInfrastructure(builder.Configuration);
-        builder.Services.AddSingleton<HttpClient>();
-        builder.Services.AddSingleton<GoogleGenerationProductionTransportFactory>();
-        builder.Services.AddSingleton<GoogleGenerationProductionRuntimeEvidenceResolver>();
-        builder.Services.AddSingleton<GoogleGenerationProductionExecutionContextResolver>();
-        builder.Services.AddSingleton<GoogleGenerationProductionSubmissionStateOwner>();
-        builder.Services.AddSingleton(serviceProvider =>
+        RegisterProductionServices(builder.Services);
+        RegisterShell(builder.Services);
+
+        return builder.Build();
+    }
+
+    private static void RegisterProductionServices(IServiceCollection services)
+    {
+        services.AddSingleton<HttpClient>();
+        services.AddSingleton<GoogleGenerationProductionTransportFactory>();
+        services.AddSingleton<GoogleGenerationProductionRuntimeEvidenceResolver>();
+        services.AddSingleton<GoogleGenerationProductionExecutionContextResolver>();
+        services.AddSingleton<GoogleGenerationProductionSubmissionStateOwner>();
+        services.AddSingleton(serviceProvider =>
             new GoogleGenerationProductionRuntimeRequestSource(
                 serviceProvider.GetRequiredService<IGoogleGenerationSpendAuthorizationStore>(),
                 serviceProvider.GetRequiredService<GoogleGenerationProductionSubmissionStateOwner>().ResolveCurrentAsync));
-        builder.Services.AddSingleton<Stage6GoogleGenerationShellBinder>();
-        builder.Services.AddSingleton<Stage7VoiceLabCatalogShellBinder>();
-        builder.Services.AddSingleton<Stage7VoiceLabAuditionShellBinder>();
-        builder.Services.AddSingleton(serviceProvider =>
+        services.AddSingleton<Stage6GoogleGenerationShellBinder>();
+        services.AddSingleton<Stage7VoiceLabCatalogShellBinder>();
+        services.AddSingleton<Stage7VoiceLabAuditionShellBinder>();
+    }
+
+    private static void RegisterShell(IServiceCollection services)
+    {
+        services.AddSingleton(serviceProvider =>
         {
             ShellViewModel viewModel = ActivatorUtilities.CreateInstance<ShellViewModel>(serviceProvider);
-            viewModel.ConfigureStage3DocumentWorkflow(
-                serviceProvider.GetRequiredService<IDocumentLibrary>(),
-                serviceProvider.GetRequiredService<DocumentAutosaveCoordinator>());
-            viewModel.ConfigureStage3ImportWorkflow(
-                serviceProvider.GetRequiredService<ILocalDocumentImporter>(),
-                serviceProvider.GetRequiredService<DocumentPreprocessor>());
-            viewModel.ConfigureStage4PricingHistory(
-                serviceProvider.GetRequiredService<IPricingCatalogHistoryStore>(),
-                serviceProvider.GetRequiredService<IPricingContractOverrideStore>(),
-                serviceProvider.GetRequiredService<IProviderAccountStore>(),
-                serviceProvider.GetRequiredService<IProviderCapabilitySnapshotStore>());
-            viewModel.ConfigureStage5GenerationDiagnostics(
-                serviceProvider.GetRequiredService<GenerationSupportBundleExportCoordinator>(),
-                currentPolicyAllowsDiagnostics: true);
-            serviceProvider.GetRequiredService<Stage6GoogleGenerationShellBinder>().Bind(
-                viewModel,
-                serviceProvider.GetRequiredService<GoogleGenerationProductionRuntimeRequestSource>().ResolveAsync);
-            serviceProvider.GetRequiredService<Stage7VoiceLabCatalogShellBinder>().Bind(viewModel);
-            serviceProvider.GetRequiredService<Stage7VoiceLabAuditionShellBinder>().Bind(viewModel);
-            Stage8RestoreRecoveryShellBinder.ConfigurePersistedRecovery(
-                viewModel,
-                serviceProvider.GetRequiredService<RestoreRecoveryExecutionCompositionFactory>(),
-                serviceProvider.GetRequiredService<RestoreRecoveryProductionConfigurationResolver>(),
-                serviceProvider.GetRequiredService<AtomicVerifiedRestoreExecutor>());
+            ConfigureWorkflows(viewModel, serviceProvider);
+            ConfigureFinalProductionStages(viewModel, serviceProvider);
             viewModel.ApplyFinalReleasePresentation();
             viewModel.ScheduleDocumentWorkspaceStart();
             viewModel.SchedulePricingHistoryStart();
             return viewModel;
         });
-        builder.Services.AddSingleton(serviceProvider =>
+        services.AddSingleton(serviceProvider =>
         {
             MainWindow window = ActivatorUtilities.CreateInstance<MainWindow>(serviceProvider);
             DocumentWindowBehavior.Attach(window);
             return window;
         });
+    }
 
-        return builder.Build();
+    private static void ConfigureWorkflows(ShellViewModel viewModel, IServiceProvider serviceProvider)
+    {
+        viewModel.ConfigureStage3DocumentWorkflow(
+            serviceProvider.GetRequiredService<IDocumentLibrary>(),
+            serviceProvider.GetRequiredService<DocumentAutosaveCoordinator>());
+        viewModel.ConfigureStage3ImportWorkflow(
+            serviceProvider.GetRequiredService<ILocalDocumentImporter>(),
+            serviceProvider.GetRequiredService<DocumentPreprocessor>());
+        viewModel.ConfigureStage4PricingHistory(
+            serviceProvider.GetRequiredService<IPricingCatalogHistoryStore>(),
+            serviceProvider.GetRequiredService<IPricingContractOverrideStore>(),
+            serviceProvider.GetRequiredService<IProviderAccountStore>(),
+            serviceProvider.GetRequiredService<IProviderCapabilitySnapshotStore>());
+        viewModel.ConfigureStage5GenerationDiagnostics(
+            serviceProvider.GetRequiredService<GenerationSupportBundleExportCoordinator>(),
+            currentPolicyAllowsDiagnostics: true);
+    }
+
+    private static void ConfigureFinalProductionStages(ShellViewModel viewModel, IServiceProvider serviceProvider)
+    {
+        serviceProvider.GetRequiredService<Stage6GoogleGenerationShellBinder>().Bind(
+            viewModel,
+            serviceProvider.GetRequiredService<GoogleGenerationProductionRuntimeRequestSource>().ResolveAsync);
+        serviceProvider.GetRequiredService<Stage7VoiceLabCatalogShellBinder>().Bind(viewModel);
+        serviceProvider.GetRequiredService<Stage7VoiceLabAuditionShellBinder>().Bind(viewModel);
+        Stage8RestoreRecoveryShellBinder.ConfigurePersistedRecovery(
+            viewModel,
+            serviceProvider.GetRequiredService<RestoreRecoveryExecutionCompositionFactory>(),
+            serviceProvider.GetRequiredService<RestoreRecoveryProductionConfigurationResolver>(),
+            serviceProvider.GetRequiredService<AtomicVerifiedRestoreExecutor>());
     }
 }
